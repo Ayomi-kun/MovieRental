dbd59ebba48331b64987589964ba0bac4d9ec198
Ayomiga <oluwayemiayo22@gmail.com>
2019-05-13 09:51:28 +0100

    ActionResults and ActionParameters

--- MovieRentalApp/Controllers/MoviesController.cs
+++ MovieRentalApp/Controllers/MoviesController.cs
@@ -14,6 +14,34 @@ namespace MovieRentalApp.Controllers
         {
             var movie = new Movie() { Name = "Lego Movie" };
             return View(movie);
+
+            //return Content("Hello World");
+
+            //return HttpNotFound();
+
+            //return new EmptyResult();
+
+            //return RedirectToAction("Index", "Home", new { page =1, sortBy= "name"});
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id =" + id);
+        }
+
+        //Movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
