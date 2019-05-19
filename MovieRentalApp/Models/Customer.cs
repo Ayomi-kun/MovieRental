875325dbd22b19e6a5e33058beddc72a4eefb3b8
Ayomiga <oluwayemiayo22@gmail.com>
2019-05-19 20:01:04 +0100

    Subscription to customerModel
    Also added migration and updated databse with PM

--- MovieRentalApp/Models/Customer.cs
+++ MovieRentalApp/Models/Customer.cs
@@ -9,5 +9,6 @@ namespace MovieRentalApp.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
     }
 }
