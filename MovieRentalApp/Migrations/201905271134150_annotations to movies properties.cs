069bccd0c451d2826772d6942137c9aa7c950e0b
Ayomiga <oluwayemiayo22@gmail.com>
2019-05-27 12:37:19 +0100

    additional properties movie model

---
+++ MovieRentalApp/Migrations/201905271134150_annotations to movies properties.cs
@@ -0,0 +1,18 @@
+namespace MovieRentalApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class annotationstomoviesproperties : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Movies", "Name", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Movies", "Name", c => c.String());
+        }
+    }
+}
--- MovieRentalApp/Models/Movie.cs
+++ MovieRentalApp/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,17 @@ namespace MovieRentalApp.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public Genre Genre { get; set; }
+        [Required]
         public byte GenreID { get; set; }
+        [Required]
+        public DateTime ReleasedDate { get; set; }
+        [Required]
+        public DateTime DateAdded { get; set; }
+        [Required]
+        public byte NumberInStock { get; set; }
+       
     }
 }
